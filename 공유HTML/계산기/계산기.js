8c132fc485eeaf568cdf8c45d0f463f0b9f8157d
JangIkIk <wkdwnsdlr37@gmail.com>
2022-08-30 14:34:08 +0900

    업데이트

--- 공유HTML/계산기/계산기.js
+++ 공유HTML/계산기/계산기.js
@@ -3,34 +3,48 @@ window.addEventListener("DOMContentLoaded",()=>{
 
 
     //  클릭함수호출
-    //  C_btns();
+     C_btns();
 
 
      //숨기기 클릭
      Z_btns();
+
+
+
+     let btn = document.querySelectorAll("#calculator #container_main .common_flex input");
+
+        btn.addEventListener('click', function(event) {
+        // 버튼을 눌렀을 때 작동하는 함수입니다.
+        comsole.log("테스트");
+
+
+        // ...
+    })
 })
 
 /****************** 로더영역 ******************/
 
 
-// function C_btns(){
-//     console.log("클릭");
+function C_btns(){
+    console.log("클릭");
 
-//     let H_screen = document.querySelector(".hd_screen input").value;
-//     console.log(`${H_screen}`);
-//     let M_btns = document.querySelectorAll(".common_flex input")
+    let H_screen = document.querySelector(".hd_screen input").value;
+    console.log(`${H_screen}`);
+    let M_btns = document.querySelectorAll(".common_flex input");
     
 
-//     M_btns.forEach((val,idx)=>{
+    M_btns.forEach((val,idx)=>{
 
-//         val.onclick=()=>{
-//             console.log(`${val.value}내용`);
-
-//         }
-//     })
+        val.onclick=()=>{
+            console.log(`${val.value} 내용`);
+            H_screen.value += "val";
 
 
-// }
+        }
+    })
+
+
+}
 
 
 function Z_btns(){
@@ -47,9 +61,10 @@ function Z_btns(){
             console.log(`${x.value}`)
             
             if( x.value === "×"){
-                cal.style.transform = 'scale(0)';
+                cal.style.right = '200px';
+                // cal.style.transform = 'scale(0)';
             } else if( x.value === "-") {
-                // cal.style.right = '100px';
+                
                 cal.style.transform ='scale(1)';
             } else if( x.value === "+"){
                 cal.style.transform = 'scale(2)'; 
@@ -58,5 +73,7 @@ function Z_btns(){
         
     }
 }
+
+
 
 
