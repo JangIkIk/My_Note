ade4b8190a706ebe59f6be0c94c56da5022517f8
Jangs <wkdwnsdlr37@gmail.com>
2022-09-19 19:20:42 +0900

    업데이트

--- z페이지네이션/부트캠프 게시판구현/script.js
+++ z페이지네이션/부트캠프 게시판구현/script.js
@@ -51,8 +51,15 @@ const convertToDiscussion = (obj) => {
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 페이지네이션 구간 ???@@@@@@@@@@@@@@@@@@@@2
 
 // data 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+
+// 8. 
+// ul = li로 만들어진 데이터들
+// pageStart = 10
+// pageEnd = 20
 const render = (element, from, to) => {
-  console.log(from, to);
+  console.log("첫번째 파라미터",element)
+  console.log("두번째 파라미터",from);
+  console.log("세번째 파라미터",to);
 
   if (!from && !to) {
     from = 0;
@@ -69,44 +76,62 @@ const render = (element, from, to) => {
 };
 
 // 페이지네이션을 위한 변수
-let limit = 10; // 화면에 보여질 리스트 개수를 10개로 할당
-let page = 1; // 왼쪽/오른쪽의 버튼을 눌렀을시에 이동을위한 값?
+let limit = 10; 
+let page = 1; 
 
 // ul 요소에 data 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul, 0, limit);
 
-const getPageStartEnd = (limit, page) => { // 화면에 렌더링할 값과
+// 3.아래 클릭이벤트가 발생한 이벤트 핸들러에서  limit = 10 page = 2로 파라미터를 받음
+const getPageStartEnd = (limit, page) => { 
   const len = data.length - 1; // 총데이터의 길이에서 -1
   console.log(len,"data.length의 길이")
+
+  // 4. 받은 파라미터에서 pageStart = (2- 1) * 10 = 10이므로 => pageStart = 10
   let pageStart = Number(page - 1) * Number(limit); 
+  // 5. 받은 파라미터에서 pageEnd = 10 + 10이므로 => pageEnd = 20
   let pageEnd = Number(pageStart) + Number(limit);
+
+
   if (page <= 0) {
     pageStart = 0;
   }
   if (pageEnd >= len) {
     pageEnd = len;
   }
+
+  // 6. pageStart = 10 , pageEnd = 20으 리턴
   return { pageStart, pageEnd };
 };
 
 const buttons = document.querySelector(".buttons"); // 패이지 넘기버튼의 부모요소에 접근
 buttons.children[0].addEventListener("click", () => { // 부모요에 클릭이벤트가 발생시에!!
   console.log(buttons.children[0],"버튼요소의 0번쨰아들?");
-  if (page > 1) { // page의 값이 1보다작다면
-    page = page - 1; // page의 값에서 -1을 해준다?
+  if (page > 1) { 
+    page = page - 1;
   }
-  // 그게 아니라면
+  
   const { pageStart, pageEnd } = getPageStartEnd(limit, page); // 
   render(ul, pageStart, pageEnd);
 });
 
 buttons.children[1].addEventListener("click", () => {
+  // 1.두번째 버튼에 클릭이벤특 발생하면!!
+  // limit 와 page의 초기값은 10 * 1
+  // data.length는 41 -1 = 40 이고, limit * page = 10이무로 f문의 조건을 만족하여 page 의 값이 2로증가
   console.log(buttons.children[1],"버튼요소의 1번쨰아들?");
   if (limit * page < data.length - 1) {
     page = page + 1;
   }
+  // 2.구조분해할당 문법을 사용하여 getPageStarEnd에서 받은 리턴값을 각각담는다!
+  // 먼저 함수에 인자를 담아 호출
   const { pageStart, pageEnd } = getPageStartEnd(limit, page);
+
+  // 7. 리턴받은 값으로 렌더 함수를 호출
+  // ul = li로 만들어진 데이터들
+  // pageStart = 10
+  // pageEnd = 20
   render(ul, pageStart, pageEnd);
 });
 
