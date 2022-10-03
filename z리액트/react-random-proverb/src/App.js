794264086b4be593353c20055140642c8a371bdc
Jangs <wkdwnsdlr37@gmail.com>
2022-10-04 00:21:00 +0900

    react switch toggle구현

--- z리액트/react-random-proverb/src/App.js
+++ z리액트/react-random-proverb/src/App.js
@@ -1,9 +1,9 @@
 //eslint-disable // 좋지않은문법오류를 나타내지않음
 //1. Route를 이용하여 버튼클릭시 페이지이동 (완료)
 //2. 버튼클릭시에 ToggleSwitch가 있는 페이지로 이동 (완료)
-//3. ToggleSwitch은 클릭시 on/off로 구현!!
-//4. ToggleSwitch가 on상태일때 3초뒤 Alert내용 : [ 그 코딩실력에 잠이와? TIL은 썼니? ]
-//   , 클릭시 on/off 색상다르게 및 글씨 보이도록!
+//3. ToggleSwitch은 클릭시 on/off로 구현!! (완료)
+//4. ToggleSwitch가 on상태일때 3초뒤 Alert내용 : [ 그 코딩실력에 잠이와? TIL은 썼니? ] (완료)
+//   , 클릭시 on/off 색상다르게 및 글씨 보이도록! (완료)
 
 import React from 'react';
 import './App.css';
@@ -20,10 +20,11 @@ function App() {
      <BrowserRouter>
         <div className='App'>
             <Link to="/Home"><button>홈으로가기</button></Link>
-            <Link to="/Main"><button>버튼보러가기</button></Link>
+            <Link to="/Main"><button>css버튼보러가기</button></Link>
             <Routes>
                 <Route path="/Home" element={<Home/>}/>
                 <Route path="/Main" element={<Main/>}/>
+
             </Routes>
         </div>
     </BrowserRouter>
--- z리액트/react-random-proverb/src/togglebtn.js
+++ z리액트/react-random-proverb/src/togglebtn.js
@@ -1,11 +1,24 @@
 import "./togglebtn.css";
+import React, {useState} from 'react';
 
 
 function Togglebtn(){
+
+    const [Alert, setAlert] = useState(false);
+    const alertEvent = () =>{ 
+        
+        if(Alert === false){
+            setTimeout(()=>{
+                alert("[ 그 코딩실력에 잠이와? TIL은 썼니? ]");
+            },1000);
+        }
+        setAlert(!Alert)
+    }
+
     return(
         <>
         <label className="toggle">
-            <input className="toggle_input" type="checkbox"/>
+            <input className="toggle_input" type="checkbox" onClick={alertEvent}/>
             <span className="toggle_span"/>
         </label>   
         </>
@@ -16,4 +29,3 @@ function Togglebtn(){
 
 export default Togglebtn;
 
-// 1. 라벨에 크기 조정
