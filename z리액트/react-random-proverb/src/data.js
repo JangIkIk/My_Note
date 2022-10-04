c76b85756ba0aa4201f603e4dd5eac45b0d2d950
Jangs <wkdwnsdlr37@gmail.com>
2022-10-04 21:49:14 +0900

    업데이트

---
+++ z리액트/react-random-proverb/src/data.js
@@ -0,0 +1,6 @@
+const data = {
+    photo: "../나무/나무1.jpeg",
+    name : "나무",
+    content : "랜덤",
+    day : "2022,22,22"
+}
--- z리액트/react-random-proverb/src/index.js
+++ z리액트/react-random-proverb/src/index.js
@@ -2,17 +2,72 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import photo from "../../namy/"
+
+
+
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// function Welcome(propss) {
+//   return <h1>Hello, {propss.name}</h1>;
+// }
+
+// const title = "변수를사용"
+
+// root.render(
+//   <div>
+//     <App/>
+//     <Welcome name={title}/> 
+//     <Welcome name="jun"/> 
+//     <Welcome name="ik"/> 
+//   </div>
+//     );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App />,
-    document.getElementById('root')
-  // </React.StrictMode>
-);
+   <div>
+    <h1>테스트중</h1>
+    <img src="../../namy/나무1.jpeg"/>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+   </div>
+
+)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // function Clock(){
+      
+    //     const time = (
+    //       <>
+    //       <h1>현재시간</h1>
+    //       <h2>{new Date().toLocaleTimeString()}</h2>
+    //       </>
+    //     )
+    //     root.render(time)
+    //   }
+    //   setInterval(Clock,1000)  
+    
+    
+    
+    
+    
+    
