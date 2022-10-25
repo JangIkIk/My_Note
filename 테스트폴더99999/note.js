467866417f75a52b9e5fef131ca9465f1c9458d9
Jangs <wkdwnsdlr37@gmail.com>
2022-10-25 21:03:00 +0900

    Update

--- 테스트폴더99999/note.js
+++ 테스트폴더99999/note.js
@@ -1,74 +1,64 @@
-const stringifiableObjects = [
-    9,
-    null,
-    true,
-    false,
-    "Hello world",
-    [],
-    [8],
-    ["hi"],
-    [8, "hi"],
-    [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999],
-    [8, [[], 3, 4]],
-    [[[["foo"]]]],
-    {},
-    { a: "apple" },
-    { foo: true, bar: false, baz: null },
-    { "boolean, true": true, "boolean, false": false, null: null },
-    // basic nesting
-    { a: { b: "c" } },
-    { a: ["b", "c"] },
-    [{ a: "b" }, { c: "d" }],
-    { a: [], c: {}, b: true },
-  ];
+function reverseArr(arr) {
+    // TODO: 여기에 코드를 작성합니다.
+  
+      if(arr.length === 0){
+        return []
+      }
 
 
-  function stringifyJSON(obj) {
-    console.log("함수실행");
-    
-    
-    if(obj.length === 0){
-        return obj
-    }
-
-
-
- 
-    
-    ///
-    let result = [];
-    for (let i = 0; i < obj.length; i++) {
-        if (typeof obj[i] === "number") {
-            result.push(`"${obj[i]}"`);
-        } else if(obj[i] === null ){
-            result.push(`"${obj[i]}"`);
-        } else if(typeof obj[i] === "boolean"){
-            result.push(`"${obj[i]}"`);
-        } else if(typeof obj[i] === 'string'){
-            result.push(`"${obj[i]}"`)
-        }
-
-        if(Array.isArray(obj[i])){
-            if(obj[i].length === 0){
-                result.push(`"${"[]"}"`)
-            } else{
-                result.push(`"${[obj[i]]}"`)
-            }
-        }
-        // else if (typeof obj[i] === 'array') {
-        //     result.push(stringifyJSON(obj[i]));
-        // }
-    }
-    
-    return `'${result}'`
-    ////
-
-    
-    
+    // console.log(`${arr.slice(1)}.concat(arr[${arr[0]}]) = ${arr.slice(1).concat(arr[0])}`)
+    const result = reverseArr(arr.slice(1)).concat(arr[0]);
+    console.log(result);
+    return result;
+  
   }
 
-  const r = stringifyJSON(stringifiableObjects);
-  console.log(r);
-  console.log(typeof r[5]);
+  let output = reverseArr([1, 2, 3]);
+console.log(output); // --> [3, 2, 1]
+  
 
-// console.log(["a",2,3,4])
+
+// const print = console.log;
+
+// function fibonacci(n, v, depth=0) {
+//   let msg = '';
+
+//   if (depth == 0) {
+//     print(`[${v}] n: ${n}`)
+//   } else {
+//     let d = ' '.repeat(depth * 2);
+//     if (v == 'L') {
+//       msg = `${n + 1} - 1`;
+//     } else {
+//       msg = `${n + 2} - 2`;
+//     }
+
+//     print(`${d}[${v}] ${msg} (${n})`)
+//   }
+
+//   if ( n <= 1 ) {
+//     return n;
+//   }
+
+//   depth++;
+//   return fibonacci(n-1, 'L', depth) + fibonacci(n-2, 'R', depth);
+// }
+
+// fibonacci(3, 'Root');
+
+/*
+
+3
+
+3 - 1 = 2;
+
+2 - 1 = 1; return 
+
+
+
+
+
+
+
+6. 
+*/
