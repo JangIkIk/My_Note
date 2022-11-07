4b8a5a29c9a7a6279368085ca8ede2e6d494e8bf
Jangs <wkdwnsdlr37@gmail.com>
2022-11-07 14:10:34 +0900

    업데이트

--- 테스트폴더99999/note.js
+++ 테스트폴더99999/note.js
@@ -1,34 +1,41 @@
-let tiling = function (n) {
-    // TODO: 여기에 코드를 작성합니다.
-    
-    const result = [0, 1, 2];
+
+
+// const largestProductOfThree = function (arr) {
   
+//   console.log("정렬전:",arr);
+
+//   // 변수에 넘겨받은 파라미터를 슬라이트로 복사후
+//   // sort로 정렬한다. 
+//   const sorted = arr.slice().sort((a, b) => {
+//   console.log("시작");
+//   console.log(`a:${a}, - b:${b}`);
+//   console.log(`a-b 의값?: ${a - b}`);
+//   return a - b
+//   });
   
-    // result = [0,1,2,3]
-    // result = [0,1,2,3,5]
+//   console.log("정렬후:",sorted);
+//   const len = arr.length;
+//   const candi1 = sorted[len - 1] * sorted[len - 2] * sorted[len - 3];
+//   const candi2 = sorted[len - 1] * sorted[0] * sorted[1];
+//   return Math.max(candi1, candi2);
+// };
+
+// // let output = largestProductOfThree([2, 1, 3, 7]);
+// // console.log(output);
+
+
+
+// let output = largestProductOfThree([-1, 2, -5, 7]);
+// console.log(output); // --> 35 (= -1 * -5 * 7)
+
+
+
+var numbers = [4, 2, 5, 1, 3];
+numbers.sort(function(a, b) {
+  console.log(`a:${a}, b${b}`);
   
-    for (let i = 3; i <= n; i++) {
-      result[i] = result[i-1] + result[i-2];
-    //   console.log(result);
-    }
-
-    console.log(result);
-    return result[n]
-  
-  };
-
-//   output = tiling(4);
-// console.log(output); // --> 5
+  return a - b;
+});
 
 
-
-let arr = [0, 1, 2, 3, 5];
-
-function add(n){
-    return n
-}
-
-let a = add(arr);
-
-console.log(a);
-  
+console.log(numbers);
